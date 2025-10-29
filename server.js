1a4e04489fafd9f54d07bdabd13a597662449026
blandine <blandinebukasa118@gmail.com>
2025-10-29 19:06:32 +0200

    Update week1

--- server.js
+++ server.js
@@ -10,6 +10,23 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const path = require("path")
+
+/* ***********************
+ * Middleware
+ *************************/
+app.use(express.static(path.join(__dirname, "public")))
+app.use(expressLayouts)
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+//app.use(static)
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+app.set("views", path.join(__dirname, "views"))
 
 /* ***********************
  * Routes
@@ -19,13 +36,6 @@ app.use(static)
 app.get("/", function (req, res) {
   res.render("index", { title: "Home" })
 })
-/* ***********************
- * View Engine and Templates
- *************************/
-//app.use(static)
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
