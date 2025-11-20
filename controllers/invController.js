3ed754facc99c8461bba8b3bb6fac736b7f936d6
blandine <blandinebukasa118@gmail.com>
2025-11-20 13:47:25 +0200

    Updata

--- controllers/invController.js
+++ controllers/invController.js
@@ -2,55 +2,86 @@
 const invModel = require("../models/inventory-model");
 const utilities = require("../utilities/");
 
-
 const invCont = {};
 
-/* ***************************
- *  Build inventory by classification view
- * ************************** */
+/* =======================================
+ * Build inventory by classification
+ * ======================================= */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classificationId
-  const data = await invModel.getInventoryByClassificationId(classification_id);
-  const grid = await utilities.buildClassificationGrid(data);
- 
-  let nav = await utilities.getNav();
-  const className = data[0].classification_name
-  res.render("./inventory/classification", {
-    title: className + " vehicles",
-    nav,
-    grid,
-    
-  });
-}
-
-/* ***************************
- *  Build inventory item detail view
- * ************************** */
-
-invCont.getInventoryItem = async function (req, res, next) {
   try {
-    const itemId = req.params.id;
-    const inventoryItem = await inventoryModel.getItemById(itemId);
+    // Validate classificationId
+    const classificationId = parseInt(req.params.classificationId);
 
-    if (!inventoryItem) {
-      const error = new Error("Item not found");
-      error.status = 404;
-      return next(error);
+    if (isNaN(classificationId)) {
+      // If invalid like "xyz"
+      let nav = await utilities.getNav();
+      return res.status(404).render("errors/error", {
+        title: "Invalid Classification",
+        nav,
+        message: "The classification you are looking for does not exist."
+      });
     }
 
-    const itemHTML = utilities.buildItemHTML(inventoryItem);
-    const nav = await utilities.getNav();
+    // Get vehicles
+    const data = await invModel.getInventoryByClassificationId(classificationId);
 
-    res.render('inventory/itemDetail', {
-      title: `${inventoryItem.inv_make} ${inventoryItem.inv_model}`,
+    if (!data || data.length === 0) {
+      let nav = await utilities.getNav();
+      return res.status(404).render("errors/error", {
+        title: "No Vehicles Found",
+        nav,
+        message: "No vehicles match this classification."
+      });
+    }
+
+    // Build grid safely
+    const grid = await utilities.buildClassificationGrid(data);
+    const className = data[0].classification_name;
+    let nav = await utilities.getNav();
+
+    return res.render("inventory/classification", {
+      title: className + " Vehicles",
       nav,
-      itemHTML
+      grid
     });
+
   } catch (error) {
     next(error);
   }
 };
 
+/* =======================================
+ * Build item detail page
+ * ======================================= */
+invCont.getItemDetail = async function(req, res, next) {
+  try {
+    const invId = parseInt(req.params.inventoryId);
 
-  module.exports = invCont;
-  
+    if (isNaN(invId)) {
+      return res.status(404).render("errors/error", {
+        message: "Invalid vehicle ID"
+      });
+    }
+
+    const item = await invModel.getItemById(invId);
+
+    if (!item) {
+      return res.status(404).render("errors/error", {
+        message: "Vehicle not found"
+      });
+    }
+
+    let nav = await utilities.getNav();
+
+    return res.render("inventory/itemDetail", {
+      title: `${item.inv_make} ${item.inv_model}`,
+      nav,
+      item
+    });
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = invCont;
